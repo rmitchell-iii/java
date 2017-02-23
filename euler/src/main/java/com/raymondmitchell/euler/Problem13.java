fc572a12a9f235962bb514aa9ea66e13b32035a6
Ray Mitchell <rmitchell@fairwaytech.com>
2017-02-22 23:32:35 -0800

    first commit

---
+++ euler/src/main/java/com/raymondmitchell/euler/Problem13.java
@@ -0,0 +1,9 @@
+package com.raymondmitchell.euler;
+
+import java.math.BigInteger;
+
+public class Problem13 {
+    public static BigInteger Solve() {
+        return new BigInteger("10");
+    }
+}
---
+++ euler/src/test/java/com/raymondmitchell/euler/Problem13Tests.java
@@ -0,0 +1,11 @@
+package com.raymondmitchell.euler;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class Problem13Tests {
+   @Test
+    public void testSolve() {
+       assertTrue(true);
+   }
+}
